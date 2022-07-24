ca66f50ea2a4c41be30b984ccb3ef0ac6966de7e
Mona-Bahri <monaeb714@gmail.com>
2022-07-24 10:12:41 +0430

    added the loacal time stamp

--- src/Localtime.js
+++ src/Localtime.js
@@ -1,11 +1,30 @@
 import React from "react";
 
 export default function Localtime(props) {
-  let day = props.date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursdays",
+    "Fridays",
+    "Saturdays",
+  ];
+  let day = days[props.date.getDay()];
+  let hours = props.date.getHours();
+  let minutes = props.date.getMinutes();
+  if (hours < 0) {
+    hours = `0${hours}`;
+  }
+  if (minutes < 0) {
+    minutes = `0{minutes}`;
+  }
   return (
     <div className="local-time">
       <div>{day}</div>
-      <div> 14:23</div>
+      <div>
+        {hours}:{minutes}
+      </div>
     </div>
   );
 }
